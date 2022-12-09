7a87289f3f68b98de93e9a3a2963e3bf6bc399b6
Borsan Ionut Razvan <razvanborsan@gmail.com>
2022-12-09 18:32:36 +0200

    Streamline highway layers generation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
 import Map, { Layer, Source } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { useEffect, useState } from "react";
 
-import { sudLot1Data } from "../data/sud_lot_1.js";
-import { sudLot2Data } from "../data/sud_lot_2.js";
-import { sudLot3Data } from "../data/sud_lot_3.js";
-import { nordLot1Data } from "../data/nord_lot_1.js";
-import { nordLot2Data } from "../data/nord_lot_2.js";
-import { nordLot3Data } from "../data/nord_lot_3.js";
-import { nordLot4Data } from "../data/nord_lot_4.js";
-import { soareluiData } from "../data/soarelui.js";
+import { highwayData } from "../data";
 
 const COORDS = {
   ROMANIA: {
@@ -24,7 +12,18 @@ const COORDS = {
 };
 
 export default function Home() {
-  console.log(process.env.NEXT_PUBLIC_MAP_ACCESS_TOKEN);
+  const mapPaint = {
+    "line-color": "#33ab84",
+    "line-width": 3,
+    "line-opacity": 0.8,
+  };
+
+  const layerBuilder = (lots) =>
+    lots.map((lot) => (
+      <Source key={lot.id} type="geojson" data={lot}>
+        <Layer type="line" paint={mapPaint} />
+      </Source>
+    ));
 
   return (
     <Map
@@ -33,78 +32,9 @@ export default function Home() {
       style={{ width: "100vw", height: "100vh" }}
       mapStyle={process.env.NEXT_PUBLIC_DEFAULT_MAP_STYLE}
     >
-      <Source type="geojson" data={sudLot1Data}>
-        <Layer
-          type="line"
-          paint={{
-            "line-color": "#33ab84",
-            "line-width": 3,
-          }}
-        />
-      </Source>
-      <Source type="geojson" data={sudLot2Data}>
-        <Layer
-          type="line"
-          paint={{
-            "line-color": "#33ab84",
-            "line-width": 3,
-          }}
-        />
-      </Source>
-      <Source type="geojson" data={sudLot3Data}>
-        <Layer
-          type="line"
-          paint={{
-            "line-color": "#33ab84",
-            "line-width": 3,
-          }}
-        />
-      </Source>
-      <Source type="geojson" data={nordLot1Data}>
-        <Layer
-          type="line"
-          paint={{
-            "line-color": "#33ab84",
-            "line-width": 3,
-          }}
-        />
-      </Source>
-      <Source type="geojson" data={nordLot2Data}>
-        <Layer
-          type="line"
-          paint={{
-            "line-color": "#33ab84",
-            "line-width": 3,
-          }}
-        />
-      </Source>
-      <Source type="geojson" data={nordLot3Data}>
-        <Layer
-          type="line"
-          paint={{
-            "line-color": "#33ab84",
-            "line-width": 3,
-          }}
-        />
-      </Source>
-      <Source type="geojson" data={nordLot4Data}>
-        <Layer
-          type="line"
-          paint={{
-            "line-color": "#33ab84",
-            "line-width": 3,
-          }}
-        />
-      </Source>
-      <Source type="geojson" data={soareluiData}>
-        <Layer
-          type="line"
-          paint={{
-            "line-color": "#33ab84",
-            "line-width": 3,
-          }}
-        />
-      </Source>
+      {layerBuilder(highwayData.a0.flat())}
+      {layerBuilder(highwayData.a1)}
+      {layerBuilder(highwayData.a2)}
     </Map>
   );
 }
