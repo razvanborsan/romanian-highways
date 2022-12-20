cb65ab0916e25e543255c14fda4ee1083ba89cf7
Borsan Ionut Razvan <razvanborsan@gmail.com>
2022-12-20 18:38:38 +0200

    Reorganize data

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Map, { Layer, Source } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 import { highwayData } from "../data";
+import { useCallback, useState } from "react";
 
 const COORDS = {
   ROMANIA: {
@@ -11,20 +12,25 @@ const COORDS = {
   },
 };
 
+const getPaint = (color: string) => ({
+  "line-color": color,
+  "line-width": 2,
+  "line-opacity": [
+    "case",
+    ["boolean", ["feature-state", "hover"], false],
+    1,
+    0.5,
+  ],
+});
+
+const layerBuilder = (lots) =>
+  lots.map((lot) => (
+    <Source key={lot.id} type="geojson" data={lot}>
+      <Layer type="line" paint={getPaint(lot.color)} />
+    </Source>
+  ));
+
 export default function Home() {
-  const mapPaint = {
-    "line-color": "#33ab84",
-    "line-width": 3,
-    "line-opacity": 0.8,
-  };
-
-  const layerBuilder = (lots) =>
-    lots.map((lot) => (
-      <Source key={lot.id} type="geojson" data={lot}>
-        <Layer type="line" paint={mapPaint} />
-      </Source>
-    ));
-
   return (
     <Map
       initialViewState={COORDS.ROMANIA}
@@ -35,6 +41,7 @@ export default function Home() {
       {layerBuilder(highwayData.a0.flat())}
       {layerBuilder(highwayData.a1)}
       {layerBuilder(highwayData.a2)}
+      {layerBuilder(highwayData.a4)}
     </Map>
   );
 }
